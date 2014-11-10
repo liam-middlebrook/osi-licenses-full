73cf7337a3baf6df900676aeb2d66b698b7f5966
Liam Middlebrook <liammiddlebrook@gmail.com>
2014-11-09 22:20:54 -0500

    Now prints out to file

--- osi_licenses_full/__init__.py
+++ osi_licenses_full/__init__.py
@@ -6,7 +6,8 @@ base_dir = os.path.dirname(__file__)
 
 def main():
     p = argparse.ArgumentParser()
-    p.add_argument('license', nargs='+', help="License to print the text of")
+    p.add_argument('license', nargs=1, help="License to print the text of")
+    p.add_argument('outfile', nargs=1, help="The File to write to")
 
     args = p.parse_args()
 
@@ -17,7 +18,14 @@ def main():
     licenseFile = os.path.join(base_dir, '../licenses/')
     licenseFile = os.path.join(licenseFile, license[0])
 
+    licenseText = None
+
     with open(licenseFile) as licenseFile:
-        print licenseFile.read()
+        licenseText =  licenseFile.read()
+    with open(args.outfile[0], 'w+') as outFile:
+        outFile.write(licenseText)
+
+    print licenseText
+
 if __name__ == '__main__':
     main()
